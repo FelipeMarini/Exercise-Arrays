199b0ef869b61d99100efa69951bbad2b98bfed8
Felipe Marini <felipemarini71188@gmail.com>
2020-11-20 11:50:24 -0300

    Exercise Arrays

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercise_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] listadecompras = {"maçã","batata","cenoura","melancia","carne","banana","leite","biscoito"};
+
+            Console.WriteLine(listadecompras[8]);
+            
+
+        }
+    }
+}
