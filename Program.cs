c85fde874455b8c3eb25658475d62d1daf8b11d8
Felipe Marini <felipemarini71188@gmail.com>
2020-11-20 12:10:05 -0300

    Exercise Arrays done

--- Program.cs
+++ Program.cs
@@ -8,7 +8,30 @@ namespace Exercise_Arrays
         {
             string[] listadecompras = {"maçã","batata","cenoura","melancia","carne","banana","leite","biscoito"};
 
-            Console.WriteLine(listadecompras[8]);
+            Console.WriteLine(listadecompras[7]);
+
+            Console.WriteLine("Trocar biscoito por qual item da lista de compras?");
+
+            Console.WriteLine();
+            
+            listadecompras[7] = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine(listadecompras[7]);
+
+            Console.WriteLine();
+
+            Console.WriteLine(listadecompras.Length);
+
+            for (int i = 0; i < listadecompras.Length; i++)
+            {
+                
+                Console.WriteLine(i);
+                Console.WriteLine(listadecompras[i]);
+            
+            
+            }
             
 
         }
